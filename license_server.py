1aa2f4840173a02377808dd182dad49b4870f2d5
Timo0304 <167033311+Timo0304@users.noreply.github.com>
2025-12-27 23:49:39 +0100

    Add files via upload

---
+++ license_server.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+AUTHORIZED_MACHINES = {
+    "GOLD_SNIPER": [
+        "5404514578303"
+    ]
+}
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    data = request.json
+
+    bot = data.get("bot")
+    machine = data.get("machine_id")
+
+    if bot in AUTHORIZED_MACHINES and machine in AUTHORIZED_MACHINES[bot]:
+        print("SERVER DEBUG → AUTHORIZED")
+        return jsonify(status="AUTHORIZED")
+
+    print("SERVER DEBUG → DENIED")
+    return jsonify(status="DENIED"), 403
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
